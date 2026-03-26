a66a7c427f1cac7b54b217f5b4f0b00930d70b52
KNODRON <juan.jimenezv08@gmail.com>
2026-03-26 10:03:49 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AQUI_TU_API_KEY",
-  authDomain: "AQUI_TU_PROYECTO.firebaseapp.com",
-  projectId: "AQUI_TU_PROJECT_ID",
-  storageBucket: "AQUI_TU_PROYECTO.firebasestorage.app",
-  messagingSenderId: "AQUI_TU_MESSAGING_SENDER_ID",
-  appId: "AQUI_TU_APP_ID"
+  apiKey: "AIzaSyCQ0TQHT4d5_yqGNcGdlD8WooBPWo_z5sw",
+  authDomain: "ptz-depto.firebaseapp.com",
+  projectId: "ptz-depto",
+  storageBucket: "ptz-depto.firebasestorage.app",
+  messagingSenderId: "916252156375",
+  appId: "1:916252156375:web:52a2b958fd5026691aeb92"
 };
 
 const app = initializeApp(firebaseConfig);
